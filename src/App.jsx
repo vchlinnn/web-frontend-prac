05dcdad46ff7b5a098be89b9c51fad81a669846e
vchlinnn <chuclinh190305@gmail.com>
2024-07-12 12:25:40 -0500

    Forms and Filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,60 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Note from './components/Note'
 /* eslint-disable react/prop-types */
 // Define a React component with the name App
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault() // prevent the default action of submitting a form 
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+
+    // Not mutate the original notes array - create new copy 
+    // NEVER MUTATE STATE DIRECTLY IN REACT
+    setNotes(notes.concat(noteObject))
+    // Reset the value of the controlled input 
+    setNewNote('')
+  }
+
+  // an event handler - handle the event of inputs changing 
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+  
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+        value={newNote}
+        onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
