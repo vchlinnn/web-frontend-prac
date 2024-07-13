ad13b651610c764bd61c694968ab2eb1d926dd31
vchlinnn <chuclinh190305@gmail.com>
2024-07-13 17:35:17 -0500

    Sending data to the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
+
 /* eslint-disable react/prop-types */
 // Define a React component with the name App
 
@@ -10,6 +12,15 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+/*
+  useEffect(() => {
     console.log('effect')
     axios
       .get('http://localhost:3001/notes')
@@ -18,14 +29,56 @@ const App = () => {
         setNotes(response.data)
       })
   }, [])
+*/
 
-  const addNote = (event) => {
-    event.preventDefault() // prevent the default action of submitting a form 
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
-    }
+const addNote = (event) => {
+  event.preventDefault() // prevent the default action of submitting a form 
+  const noteObject = {
+    content: newNote,
+    important: Math.random() < 0.5,
+    // id: String(notes.length + 1),
+  }
+
+  noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = id => {
+    // const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+    .update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+    .catch(error => {
+      console.log(error)
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })
+}
+
+
+
+/*
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      // The new note returned by the backend server is added to the list of notes
+      setNotes(notes.concat(response.data))
+      // Then reset the note creation form 
+      setNewNote('')
+    })
+}
+
 
     // Not mutate the original notes array - create new copy 
     // NEVER MUTATE STATE DIRECTLY IN REACT
@@ -33,10 +86,11 @@ const App = () => {
     // Reset the value of the controlled input 
     setNewNote('')
   }
+*/
 
   // an event handler - handle the event of inputs changing 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
@@ -44,28 +98,32 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important === true)
   
-  return (
-    <div>
-      <h1>Notes</h1>
+    return (
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
+        <h1>Notes</h1>
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all' }
+          </button>
+        </div>      
+        <ul>
+          {notesToShow.map(note => 
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          )}
+        </ul>
+        <form onSubmit={addNote}>
+          <input
+            value={newNote}
+            onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+        </form> 
       </div>
-      <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input 
-        value={newNote}
-        onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
-    </div>
-  )
-}
+    )
+  }
 
 export default App
